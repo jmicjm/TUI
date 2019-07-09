c003abe3922be737c204460ef87b41f6c120e327
Jakub Michalski <jakub.michalski2000@outlook.com>
2019-07-09 22:37:04 +0200

    tui::input_text changes

--- src/tui_input_text.h
+++ src/tui_input_text.h
@@ -36,38 +36,43 @@ namespace tui
 		int m_cursor_pos_in_txt = 0;
 		time_frame m_cursor_blink;
 
-		bool redraw_needed = true;
+		bool m_redraw_needed = true;
 
 		void updateCursorPos()
 		{
+			vec2i old_pos = m_cursor_pos;
+
 			if (m_text.getText().size() > 0)
 			{
-				auto setPos = [&]()
+				auto getRelativePos = [&]()
 				{
-					m_cursor_pos = m_text.getSymbolPos(m_cursor_pos_in_txt);
+					vec2i pos = m_text.getSymbolPos(m_cursor_pos_in_txt);
+					pos.y -= m_text.getLine();
 
-					m_cursor_pos.y -= m_text.getLine();
+					return pos;
 				};
 
-				setPos();
+				m_cursor_pos = getRelativePos();
 
 				if (m_cursor_pos.y >= getSize().y)
 				{
 					int c = m_cursor_pos.y - (getSize().y-1);
 
 					m_text.goToLine(m_text.getLine()+c);
-					setPos();
+					m_cursor_pos = getRelativePos();
 				}
 				else if (m_cursor_pos.y < 0)
 				{
 					int c = m_cursor_pos.y * -1;
 
 					m_text.goToLine(m_text.getLine()-c);
-					setPos();
+					m_cursor_pos = getRelativePos();
 				}
 
 			}
 			else { m_cursor_pos = { 0,0 }; }
+
+			if (m_cursor_pos != old_pos) { m_redraw_needed = true; }
 		}
 
 		void updateText()
@@ -75,25 +80,26 @@ namespace tui
 			console_string s = m_str;
 			s += " ";
 
+			if (m_cursor_pos_in_txt > s.size()) { m_cursor_pos_in_txt = s.size(); }
+
 			m_text.setText(s);
 		}
 		
-		void moveCursorRight(bool set_str)
+		void moveCursorRight()
 		{
-			if (set_str) { updateText(); }
-
-			if (m_cursor_pos_in_txt < m_str.size()) { m_cursor_pos_in_txt++; }
-
-			updateCursorPos();
-
+			if (m_cursor_pos_in_txt < m_str.size()) 
+			{ 
+				m_cursor_pos_in_txt++; 
+				m_redraw_needed = true;
+			}
 		}
-		void moveCursorLeft(bool set_str)
+		void moveCursorLeft()
 		{
-			if (set_str) { updateText(); }
-
-			if (m_cursor_pos_in_txt > 0) { m_cursor_pos_in_txt--; }
-
-			updateCursorPos();
+			if (m_cursor_pos_in_txt > 0) 
+			{ 
+				m_cursor_pos_in_txt--; 
+				m_redraw_needed = true;
+			}
 		}
 
 		void moveCursorUp()
@@ -102,16 +108,15 @@ namespace tui
 			{
 				vec2i c_pos = m_text.getSymbolPos(m_cursor_pos_in_txt);
 
-				for (int l_pos = m_cursor_pos_in_txt; l_pos >= 0; --l_pos)
+				for (int l_pos = m_cursor_pos_in_txt-1; l_pos >= 0; --l_pos)
 				{
 					if (m_text.getSymbolPos(l_pos).y == c_pos.y - 1 && m_text.getSymbolPos(l_pos).x <= c_pos.x)
 					{
 						m_cursor_pos_in_txt = l_pos;
+						m_redraw_needed = true;
 						break;
 					}
 				}
-
-				updateCursorPos();
 			}
 		}
 
@@ -121,37 +126,29 @@ namespace tui
 			{
 				vec2i c_pos = m_text.getSymbolPos(m_cursor_pos_in_txt);
 
-				for (int l_pos = m_cursor_pos_in_txt; l_pos <= m_str.size(); ++l_pos)
+				for (int l_pos = m_cursor_pos_in_txt+1; l_pos <= m_str.size(); ++l_pos)
 				{
 					if (m_text.getSymbolPos(l_pos).y == c_pos.y + 1 && (m_text.getSymbolPos(l_pos).x == c_pos.x || l_pos == m_str.size()))
 					{
 						m_cursor_pos_in_txt = l_pos;
+						m_redraw_needed = true;
 						break;
 					}
 					else if (m_text.getSymbolPos(l_pos).y > c_pos.y + 1) 
 					{
 						m_cursor_pos_in_txt = l_pos - 1;
+						m_redraw_needed = true;
 						break; 
 					}
 				}
-
-				updateCursorPos();
 			}
-		}
-
-		int getTextWidth()
-		{
-			int w = m_text.getSize().x;
-			if (m_text.isDisplayingScroll()) { w--; }
-			return w;
 		}
 
 		void fill()
 		{
-			if (redraw_needed)
+			if (m_redraw_needed)
 			{
 				makeTransparent();
-
 				insertSurface(m_text);
 			}
 
@@ -184,61 +181,54 @@ namespace tui
 			bool update_text = false;
 
 			std::string input = KEYBOARD::getInputAsString();
-
 			
 			for (int i = 0; i < input.size(); i++)
 			{
 				m_str.insert(m_str.begin() + m_cursor_pos_in_txt, input[i]);
-				moveCursorRight(true);
+				updateText();
+				moveCursorRight();
 
-				redraw_needed = true;
 				update_text = true;
 			}
 			
 			for(int i = 0;(i< KEYBOARD::isKeyPressed(KEYBOARD::KEY::BACKSPACE) && m_cursor_pos_in_txt >0);i++)
 			{ 
 				m_str.erase(m_str.begin() + m_cursor_pos_in_txt - 1);
-				moveCursorLeft(true);
+				updateText();
+				moveCursorLeft();
 
-				redraw_needed = true;
 				update_text = true;
 			}
 
-			if (KEYBOARD::isKeyPressed(keyLeft)) 
-			{
-				redraw_needed = true;
-				moveCursorLeft(false); 
-			}
-			if (KEYBOARD::isKeyPressed(keyRight)) 
-			{
-				redraw_needed = true;
-				moveCursorRight(false); 
-			}
-			if (KEYBOARD::isKeyPressed(keyUp))
-			{
-				redraw_needed = true;
-				moveCursorUp();
-			}
-			if (KEYBOARD::isKeyPressed(keyDown))
-			{
-				redraw_needed = true;
-				moveCursorDown();
-			}
+			if (KEYBOARD::isKeyPressed(keyLeft)) { moveCursorLeft(); }
+			if (KEYBOARD::isKeyPressed(keyRight)) { moveCursorRight(); }
+			if (KEYBOARD::isKeyPressed(keyUp)) { moveCursorUp(); }
+			if (KEYBOARD::isKeyPressed(keyDown)) { moveCursorDown(); }
 
 			if (update_text) { updateText(); }
-		
-			fill(); 
 		}
 
-		console_string getText() { return m_text.getText(); }
+		console_string getText() { return m_str; }
 
-		void resize_action() override { redraw_needed = true; }
-		void draw_action() override { update(); }
+		void resize_action() override { m_redraw_needed = true; }
+		void draw_action() override 
+		{ 
+			update();
+			fill();
+		}
 
-		void activation_action() override {}
-		void disactivation_action() override {}
+		void activation_action() override 
+		{
+			m_text.activate();
+			m_redraw_needed = true;
+		}
+		void disactivation_action() override 
+		{
+			m_text.disactivate();
+			m_redraw_needed = true;
+		}
 
-		void setAppearance_action() override { fill(); }
+		void setAppearance_action() override { m_redraw_needed = true; }
 
 	};
 
