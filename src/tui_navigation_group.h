e85719073d93cb51b6739094a3ed657f07d63b2e
Jakub Michalski <jakub.michalski2000@outlook.com>
2019-11-08 12:10:28 +0100

    tui::navigation_group

--- src/tui_navigation_group.h
+++ src/tui_navigation_group.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "tui_utils.h"
 #include "tui_active_element.h"
+#include "tui_input.h"
 
 #include <vector>
 
@@ -10,13 +11,8 @@ namespace tui
 	struct navigation_group : active_element
 	{
 	private:
-		std::vector<int> m_key_combo_next;
 		std::vector<active_element*> m_elements;
-
-		bool m_was_next_pressed = false;
-
 		int m_selected = -1;
-
 		time_frame m_time_limit;
 
 
@@ -29,13 +25,10 @@ namespace tui
 		}
 
 	public:
+		int key_next = -1;
 
 		navigation_group() : m_time_limit(std::chrono::milliseconds(300)) {}
 
-		void setKeyComboNext(std::vector<int> combo)
-		{
-			m_key_combo_next = combo;
-		}
 		void addElement(active_element& element)
 		{
 			m_elements.push_back(&element);
@@ -45,47 +38,25 @@ namespace tui
 		{
 			if (isActive())
 			{
-
-				int act = 0;
-
-				for (int i = 0; i < m_key_combo_next.size(); i++)
+				if (tui::KEYBOARD::isKeyPressed(key_next))
 				{
-					//if (KEYBOARD::isKeyPressed(m_key_combo_next[i], TUI_BUFFERED_INPUT))
-					{
-						act++;
-					}
-				}
-				if (act == m_key_combo_next.size())
-				{
-					m_was_next_pressed = true;
-				}
+					disactivateAll();
 
-				if (m_time_limit.isEnd(true))
-				{
-
-
-					if (m_was_next_pressed)
-					{
-						disactivateAll();
-
-						if (m_selected < (int)m_elements.size() - 1) { m_selected++; }
-						else { m_selected = 0; }
-						m_elements[m_selected]->activate();
-
-						m_was_next_pressed = false;
-					}
+					if (m_selected < (int)m_elements.size() - 1) { m_selected++; }
+					else { m_selected = 0; }
+					m_elements[m_selected]->activate();
 				}
 			}
 		}
 
-		void disactivation_action() { disactivateAll(); }
-		void activation_action()
+		void disactivationAction() { disactivateAll(); }
+		void activationAction()
 		{
 			disactivateAll();
 			m_selected = 0;
 			if (m_elements.size() > 0)
 			{
-				m_elements[0]->activate();
+				m_elements[m_selected]->activate();
 			}
 		}
 
