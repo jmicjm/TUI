f40c0e4822e668f137a72be55d6b00fbade190e4
Jakub Michalski <jakub.michalski2000@outlook.com>
2019-11-09 13:17:21 +0100

    tui::bar added options to display value labels

--- src/tui_bar.h
+++ src/tui_bar.h
@@ -57,30 +57,63 @@ namespace tui
 		float m_max;
 		float m_value;
 
+		bool m_display_labels = false;
+		bool m_display_min_label = true;
+		bool m_display_max_label = true;
+		bool m_display_labels_at_end = false;
+		int m_labels_precison = -1;
+
 		void fill()
 		{
+			console_string val_str; 
+			
+			if (m_display_labels)
+			{
+				if (m_display_min_label) { val_str += ToStringP(m_min, m_labels_precison) + '/'; }
+				val_str += ToStringP(m_value, m_labels_precison); 
+				if (m_display_max_label) { val_str += '/' + ToStringP(m_max, m_labels_precison); }
+			}
+
+			auto getBarSize = [&]()
+			{
+				int s = surface1D<direction>::getSize() - val_str.size();
+				return s >= 0 ? s : 0;
+			};
+
+			int bar_offset = val_str.size() * !m_display_labels_at_end;
+			int str_offset = getBarSize() * m_display_labels_at_end;
+
+
 			float distance = fabs(m_min - m_max);
 			if (distance > 0)
 			{
-				int halves = surface1D<direction>::getSize() * 2;
+				int halves = getBarSize() * 2;
 				int h = round(fabs(m_min - m_value) / distance * halves);
 
-				int dir_c = (direction == tui::DIRECTION::HORIZONTAL ? 0 : surface1D<direction>::getSize() - 1);
+				int dir_c = (direction == tui::DIRECTION::HORIZONTAL ? 0 : getBarSize() - 1);
 
 				for (int i = 0; i < halves; i += 2)
 				{
 					if (i < h && i + 1 < h)
 					{
-						surface1D<direction>::setSymbolAt(full, abs(dir_c - i / 2));
+						surface1D<direction>::setSymbolAt(full, abs(dir_c - i / 2) + bar_offset);
 					}
 					else if (i < h && i + 1 >= h)
 					{
-						surface1D<direction>::setSymbolAt(half, abs(dir_c - i / 2));
+						surface1D<direction>::setSymbolAt(half, abs(dir_c - i / 2) + bar_offset);
 					}
 					else
 					{
-						surface1D<direction>::setSymbolAt(empty, abs(dir_c - i / 2));
+						surface1D<direction>::setSymbolAt(empty, abs(dir_c - i / 2) + bar_offset);
 					}
+				}
+			}
+
+			if (m_display_labels)
+			{
+				for (int i = 0; i < val_str.size() && i + str_offset < surface1D<direction>::getSize(); i++)
+				{
+					surface1D<direction>::setSymbolAt(val_str[i], i + str_offset);
 				}
 			}
 		}
@@ -119,6 +152,41 @@ namespace tui
 			fill();
 		}
 		float getValue() { return m_value; }
+
+		void displayLabels(bool display)
+		{
+			m_display_labels = display;
+			fill();
+		}
+		bool isDisplayingLabels() { return m_display_labels; }
+
+		void displayMinLabel(bool display)
+		{
+			m_display_min_label = display;
+			fill();
+		}
+		bool isDisplayingMinLabel() { return m_display_min_label; }
+
+		void displayMaxLabel(bool display)
+		{
+			m_display_max_label = display;
+			fill();
+		}
+		bool isDisplayingMaxLabel() { return m_display_max_label; }
+
+		void displayLabelsAtEnd(bool display)
+		{
+			m_display_labels_at_end = display;
+			fill();
+		}
+		bool isDisplayingLabelsAtEnd() { return m_display_labels_at_end; }
+
+		void setLabelsPrecision(int precision)
+		{
+			m_labels_precision = precision;
+			fill();
+		}
+		int getLabelsPrecision() { return m_labels_precision; }
 
 		void drawAction() override {};
 		void resizeAction() override { fill(); }
