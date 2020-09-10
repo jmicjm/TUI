c15efa4f67388b5000be1961cd66a0e51e58397b
Jakub Michalski <jakub.michalski2000@outlook.com>
2020-09-10 14:28:17 +0200

    changed animation

--- src/tui_animation.h
+++ src/tui_animation.h
@@ -12,43 +12,39 @@ namespace tui
 	private:
 		time_frame m_fps_control;
 		std::vector<surface> m_frames;
-		int m_frame = 0;
+		int m_frame = -1;
 
 		void fill()
 		{
-			if (m_frames.size() <= 0) { return; }
+			if (m_frames.size() == 0) { return; }
 
 			if (m_fps_control.isEnd(true)) 
 			{
-				makeTransparent(); 
+				m_frame < m_frames.size() - 1 ? m_frame++ : m_frame = 0;
 
+				clear(); 
 				insertSurface(m_frames[m_frame]);
-
-				if (m_frame < m_frames.size() - 1)
-				{
-					m_frame++;
-				}
-				else
-				{
-					m_frame = 0;
-				}
 			}
 		}
 		void drawAction() override { fill(); }
 	public:
-		void addFrame(surface surf)
-		{
-			m_frames.push_back(surf);
-		}
+		animation() {}
+		animation(surface frame) : m_frames({frame}) {}
+		animation(std::vector<surface> frames) : m_frames(frames) {}
+
 		void setFrames(std::vector<surface> frames)
 		{
 			m_frames = frames;
-			m_frame = 0;
+			m_frame = -1;
 		}
-		void clear() 
+		std::vector<surface> getFrames() { return m_frames; }
+
+		//returns index of current frame
+		int getFrame() { return m_frame; }
+
+		void addFrame(surface frame)
 		{
-			m_frames.clear();
-			m_frame = 0;
+			m_frames.push_back(frame);
 		}
 		void setFPS(unsigned int fps) { m_fps_control.setFrameTime(std::chrono::milliseconds(1000)/ fps); }
 	};
