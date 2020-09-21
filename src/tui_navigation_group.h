cea6a40bb85cf717f3509ac901eb8bee48b2d6c8
Jakub Michalski <jakub.michalski2000@outlook.com>
2020-09-21 21:31:20 +0200

    navigation_group: added block_function

--- src/tui_navigation_group.h
+++ src/tui_navigation_group.h
@@ -4,10 +4,19 @@
 
 #include <vector>
 #include <algorithm>
+#include <functional>
 
 namespace tui
 {
-	struct navigation_group : active_element, private std::vector<active_element*>
+	struct navigation_group_entry
+	{
+		active_element* element;
+		std::function<bool()> block_function;
+
+		navigation_group_entry(active_element* element, std::function<bool()> block_function = nullptr) : element(element), block_function(block_function) {}
+	};
+
+	struct navigation_group : active_element, private std::vector<navigation_group_entry>
 	{
 	private:
 		size_t m_selected = 0;
@@ -16,9 +25,9 @@ namespace tui
 		{
 			for (int i = 0; i < size(); i++)
 			{
-				if ((*this)[i]->isActive() && i != except)
+				if ((*this)[i].element->isActive() && i != except)
 				{
-					(*this)[i]->disactivate();
+					(*this)[i].element->disactivate();
 				}
 			}
 		}
@@ -27,46 +36,58 @@ namespace tui
 			m_selected = size() > 0 ? std::min(m_selected, size()-1) : 0;
 			disable(m_selected);
 
-			if (size() > 0 && !(*this)[m_selected]->isActive())
+			if (size() > 0 && !(*this)[m_selected].element->isActive())
 			{
-				(*this)[m_selected]->activate();
+				(*this)[m_selected].element->activate();
 			}
+		}
+
+		bool isBlocked()
+		{
+			if ((*this)[m_selected].block_function)
+			{
+				return (*this)[m_selected].block_function();
+			}
+			return false;
 		}
 
 		void disactivationAction() override { disable(); }
 		void activationAction() override { enable(); }
 	public:
-		short key_next = input::KEY::CTRL_RIGHT;
-		short key_prev = input::KEY::CTRL_LEFT;
+		short key_next = input::KEY::RIGHT;
+		short key_prev = input::KEY::LEFT;
 
 		navigation_group() {}
-		navigation_group(active_element* element) : std::vector<active_element*>({ element }) {}
-		navigation_group(std::vector<active_element*> elements) : std::vector<active_element*>(elements) {}
+		navigation_group(navigation_group_entry element) : std::vector<navigation_group_entry>({ element }) {}
+		navigation_group(std::vector<navigation_group_entry> elements) : std::vector<navigation_group_entry>(elements) {}
 
-		using std::vector<active_element*>::size;
-		using std::vector<active_element*>::operator[];
-		using std::vector<active_element*>::push_back;
-		using std::vector<active_element*>::pop_back;
-		using std::vector<active_element*>::insert;
-		using std::vector<active_element*>::begin;
-		using std::vector<active_element*>::end;
-		using std::vector<active_element*>::clear;
-		using std::vector<active_element*>::erase;
+		using std::vector<navigation_group_entry>::size;
+		using std::vector<navigation_group_entry>::operator[];
+		using std::vector<navigation_group_entry>::push_back;
+		using std::vector<navigation_group_entry>::pop_back;
+		using std::vector<navigation_group_entry>::insert;
+		using std::vector<navigation_group_entry>::begin;
+		using std::vector<navigation_group_entry>::end;
+		using std::vector<navigation_group_entry>::clear;
+		using std::vector<navigation_group_entry>::erase;
 
 		void update()
 		{
 			if (isActive())
 			{		
-				if (input::isKeyPressed(key_next))
+				if (!isBlocked())
 				{
-					m_selected < size() - 1 ? m_selected++ : m_selected = 0;
+					if (input::isKeyPressed(key_next))
+					{
+						m_selected < size() - 1 ? m_selected++ : m_selected = 0;
+					}
+					if (input::isKeyPressed(key_prev))
+					{
+						m_selected > 0 ? m_selected-- : m_selected = size() - 1;
+					}
+
+					enable();
 				}
-				if (input::isKeyPressed(key_prev))
-				{
-					m_selected > 0 ? m_selected-- : m_selected = size()-1;					
-				}			
-				
-				enable();
 			}
 		}
 	};
